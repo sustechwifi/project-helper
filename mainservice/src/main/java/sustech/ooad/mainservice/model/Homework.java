d6c43e54e35be80d390832fb260b1cf710b8b475
Xly777 <1435627567@qq.com>
2023-11-30 20:52:06 +0800

    添加作业实体类

---
+++ mainservice/src/main/java/sustech/ooad/mainservice/model/Homework.java
@@ -0,0 +1,49 @@
+package sustech.ooad.mainservice.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "homework")
+public class Homework {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "name", length = Integer.MAX_VALUE)
+    private String name;
+
+    @Column(name = "attachment", length = Integer.MAX_VALUE)
+    private String attachment;
+
+    @Column(name = "ddl", length = Integer.MAX_VALUE)
+    private String ddl;
+
+    @Column(name = "description", length = Integer.MAX_VALUE)
+    private String description;
+
+    @Column(name = "isgroup")
+    private Integer isgroup;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "courseid")
+    private Course courseid;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userid")
+    private AuthUser userid;
+
+}

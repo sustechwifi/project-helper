54d9b792e43e9adae35fe4c5b90b73bd2f337ed9
Yours <2587433598@qq.com>
2024-01-03 19:53:44 +0800

    新增通过指定ip，注册到 nacos

--- mainservice/docker-deploy.sh
+++ mainservice/docker-deploy.sh
@@ -1,4 +1,4 @@
 docker rm -f ooad-server-1 && docker rmi -f project-helper-mainservice
 docker build -t project-helper-mainservice .
-docker run -d -p 8070:8070 --name ooad-server-1 project-helper-mainservice
+docker run -d -p 8070:8070 -e SERVER_IP=139.199.172.27 --name ooad-server-1 project-helper-mainservice
 docker logs -f ooad-server-1
